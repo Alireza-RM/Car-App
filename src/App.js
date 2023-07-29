97b89d22b13ab70d752c4c5a308a25ca90efcd19
Alireza Alaei <alaei20ali@gmail.com>
2023-07-29 10:14:12 +0330

    add component scrollToTop in change path

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import RentalCar from "./components/RentalCar";
 import UserPanel from "./components/UserPanel";
 import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <div className="bg-gray-100">
       <Header />
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/userpanel" element={<UserPanel />} />
